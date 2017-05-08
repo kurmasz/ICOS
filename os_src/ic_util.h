eb52844c64365bc5629bcde5a8e6b3f9f7251b1b
Zachary Kurmas <kurmasz@users.noreply.github.com>
2017-05-08 09:43:41 -0400

    Changed output type of rdtsc to uint32_t.  Moved sample kernel main code to usr_src.  Removed time_branches.c from repo

--- os_src/ic_util.h
+++ os_src/ic_util.h
@@ -3,14 +3,36 @@
 
 #include "icos.h"
 
-unsigned long ic_rdtsc();  // defined in ic_util_asm.s
+///////////////////////////////////////////////////////////////////
+//
+// Functions defined in ic_util_asm.s
+//
+/////////////////////////////////////////////////////////////////
+uint32_t ic_rdtsc();  
 
+
+
+
+///////////////////////////////////////////////////////////////////
+//
+// Functions defined in ic_util.c
+//
+/////////////////////////////////////////////////////////////////
 size_t ic_strlen(const char* str);
 uint32_t ic_random();
 void ic_randomize(uint32_t seed);
 
+
+// This may be helpful for debugging.  The members of this struct are
+// very generic, because the needs will vary depending on the specific
+// problem being investigated.
 typedef struct {
-  int vga_has_failed;
+  unsigned int d1;
+  unsigned int d2;
+  unsigned int d3;
+  char* p1;
+  char* p2;
+  char* p3;
 } global_debug_data_t;
 extern global_debug_data_t gdd;
 void initialize_gdd();
--- os_src/ic_util_asm.s
+++ os_src/ic_util_asm.s
@@ -4,6 +4,9 @@
 	.globl ic_rdtsc
 
 ic_rdtsc:
+	// rdtsc places its results in %eax and %edx.  The least
+	// significant bits go in %eax; which happens to be the
+	// convential locaiton for return values.
 	rdtsc
 	ret
 	
